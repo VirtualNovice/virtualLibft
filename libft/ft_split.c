a511ec781f82f2d37e0f877fbcefca9697150af0
VirtualNovice <44501663+VirtualNovice@users.noreply.github.com>
2021-11-01 23:38:33 +0400

    Add files via upload

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -1,64 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oumali <oumali@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/01 21:48:09 by oumali            #+#    #+#             */
+/*   Updated: 2021/11/01 23:36:14 by oumali           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-int strcount(char const *s, char c)
+static	int	ft_strcount(char const *s, char c)
 {
-    int a = 0;
-    int count = 0;
-    int flag = 0;
-    
-    while(s[a] != '\0')
-    {   
-        while(s[a] != c && s[a] != '\0' )
-        {
-          flag = 1;
-          a++;
-        }
-        if (flag == 1)
-        {
-          count++;
-          flag = 0;
-          continue;
-        }
-      a++;      
-    }
-    return count;
+	int	a;
+	int	count;
+	int	flag;
+
+	a = 0;
+	count = 0;
+	flag = 0;
+	while (s[a] != '\0')
+    {
+		while (s[a] != c && s[a] != '\0')
+		{
+			flag = 1;
+			a++;
+		}
+		if (flag == 1)
+		{
+			count++;
+			flag = 0;
+			continue ;
+		}
+		a++;
+	}
+	return (count);
 }
-char **ft_split(char const *s, char c)
+
+char	**ft_split(char const *s, char c)
 {
-    int noOfStr = 0;
-    int a = 0;
-    char **split;
-    char *str2 = (char *)s;
-    int strtString = 0;
-    int strsize;
-    int strCount = 0;
-    
+	int		a;
+	char	**split;
+	int		strtstring;
+	int		strsize;
+    int		strcount;
 
-    if(s == NULL)
-        return NULL;
-
-    noOfStr = strcount(s,c);
-        
-   split = (char **)malloc((noOfStr + 1) * sizeof(char*));
-   if (split == NULL)
-        return NULL;
-   while(str2[a] != '\0')
-   {
-        strsize = 0;
-        while(str2[a] == c)
-            a++;
-        if (str2[a] != c && str2[a] != '\0')
-        {
-            strtString  = a;
-            strCount++;
-            while(str2[a] != c && str2[a] != '\0')
-          {
-            strsize++;
-            a++;
-          }
-          split[strCount - 1] = ft_substr(str2,strtString,strsize);
-        }
-   } 
-    split[strCount] = NULL;
-    return (split);
+	a = 0;
+	strtstring = 0;
+	strcount = 0;
+	if (s == NULL)
+		return (NULL);
+	split = (char **)malloc((ft_strcount(s, c) + 1) * sizeof(char*));
+	if (split == NULL)
+	   	return (NULL);
+	while (s[a] != '\0')
+	{
+		strsize = 0;
+		while (s[a] == c)
+			a++;
+		if (s[a] != c && s[a] != '\0')
+		{
+			strtstring = a;
+			strcount++;
+			while (s[a] != c && s[a] != '\0')
+			{
+				strsize++;
+				a++;
+			}
+			split[strcount - 1] = ft_substr(s, strtstring, strsize);
+		}
+	}
+	split[strcount] = NULL;
+	return (split);
 }
